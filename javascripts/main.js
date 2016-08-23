dd34e5f78ab8afcee325fa1748861b8613ede14a
Max Amey <maxwellamey@gmail.com>
2016-08-23 00:50:39 -0500

    Added javascript to prompt the modal and to exit out of it. Added extra styling.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,32 @@
 $(function(){
 
-var $thumbnailEl = $("[data-js='thumbnail']");
-var $containerEl = $("[data-js='photo']");
-var $modalEl = $("[data-js='modal']");
-var $modalImageEl = $("[data-js='modal__image']");
-var $modalTitleEl = $("[data-js='modal__title']");
-var $modalExitEl = $("[data-js='modal__exit']");
+  var $thumbnailEl = $("[data-js='thumbnail']");
+  var $photoEl = $("[data-js='photo']");
+  var $modalEl = $("[data-js='modal']");
+  var $modalImageEl = $("[data-js='modal__image']");
+  var $modalTitleEl = $("[data-js='modal__title']");
+  var $modalExitEl = $("[data-js='modal__exit']");
 
-$thumbnailEl.on("click", function{
+  $photoEl.on("click", function(e){
+    var $imageSource = $(e.currentTarget.children[0]).attr("src");
+    var $imageInfoTitle = $(e.currentTarget.children[1]).text();
+    $modalEl.toggleClass("modal__visible");
+    $modalImageEl.attr("src", $imageSource);
+    $modalEl.prepend(`
+      <h2 class="modal__title">
+        ${$imageInfoTitle}
+      </h2>
+    `);
+  });
 
-});
+  $modalExitEl.on("click", function(e){
+    $("div h2").remove();
+    $modalImageEl.attr("src", "");
+    $modalEl.toggleClass("modal__visible");
+  });
 
-
-
-
+  // $("[data-js='modalImageEl']").on("mouseover", function(e){
+  //
+  // });
 
 });
