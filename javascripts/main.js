b687d8320bdbb8f233e54e1bf38037ef2da7ab90
Max Amey <maxwellamey@gmail.com>
2016-08-22 21:24:35 -0500

    Added hover effects for the thumbnails and images for hover to toggle with js.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,14 @@
 $(function(){
 
 var $thumbnailEl = $("[data-js='thumbnail']");
+var $containerEl = $("[data-js='photo']");
 var $modalEl = $("[data-js='modal']");
+var $modalImageEl = $("[data-js='modal__image']");
+var $modalTitleEl = $("[data-js='modal__title']");
+var $modalExitEl = $("[data-js='modal__exit']");
 
 $thumbnailEl.on("click", function{
-  
+
 });
 
 
